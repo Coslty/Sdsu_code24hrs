85eab9fc211902b97312f32877a82ff0c81e4ccf
Keng <kengyin123@gmail.com>
2016-11-18 19:37:34 -0800

    update

--- src/main/java/com/example/keng/sdsu_code24hrs/GoogleMapInfoActivity.java
+++ src/main/java/com/example/keng/sdsu_code24hrs/GoogleMapInfoActivity.java
@@ -1,21 +1,34 @@
 package com.example.keng.sdsu_code24hrs;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created by Keng on 11/18/2016.
- */
+public class GoogleMapInfoActivity extends FragmentActivity implements OnMapReadyCallback {
 
-public class GoogleMapInfoActivity extends Activity {
+    private GoogleMap mMap;
 
-
-    protected void onCreate (Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_google_map_info);  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map_info);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-
-
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
